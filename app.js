69315603a8657be48d2954a6de2373c0119f432a
blandine <blandine.descamps@gmail.com>
2015-03-11 14:23:06 +0100

    add a new view and two states

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise("home");
+	$stateProvider.state('home', { 
+        url:'/',
+        templateUrl: 'home.html',
+		controller:['$scope','$state', function($scope, $state){
+			$scope.goToUsers = function () {
+				$state.go('user');
+			}
+		}]
+    }).state('user', {
+		url:'/user',
+        templateUrl: 'user.html',
+		controller: 'UserController'
+    });
+}]);
 			
-myApp.controller('MyController', function($scope){
+myApp.controller('UserController', ['$scope', '$state',function($scope, $state){
 	$scope.users = [];
 	
 	$scope.addUser = function (user) {
 		$scope.users.push(angular.copy(user));
 	}
-});
+	
+	$scope.backToHome = function () {
+		$state.go('home');
+	}
+}]);
